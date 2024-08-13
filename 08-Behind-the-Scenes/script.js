41a785cc98442b80838cecaa1e7adadccc978fbc
VitaliiDiakiv <vitalii.diakiv.it@gmail.com>
2024-08-13 14:09:30 +0300

    Section-8

---
+++ 08-Behind-the-Scenes/script.js
@@ -0,0 +1,135 @@
+'use strict';
+
+// function calcAge(birthYear) {
+//   const age = 2037 - birthYear;
+
+//   function printAge() {
+//     let output = `${firstName} is ${age}, born in ${birthYear}`;
+//     console.log(output);
+
+//     if (birthYear >= 1991 && birthYear <= 2003) {
+//       var millenial = true;
+//       const firstName = 'Vitaliiiiiiiiiiiiii';
+//       const str = ` Oh, and u are millenial, ${firstName}`;
+//       console.log(str);
+
+//       function add(a, b) {
+//         return a + b;
+//       }
+//       output = 'NEW OUT!';
+//     }
+//     console.log(millenial);
+//     // console.log(add(2, 3));
+//     console.log(output);
+//   }
+//   printAge();
+//   return age;
+// }
+
+// const firstName = 'Vitalii';
+
+// calcAge(2003);
+
+// console.log(me);
+// // console.log(job);
+// // console.log(year);
+
+// var me = 'Vitalii';
+// let job = 'Unemployed';
+// const year = 2003;
+
+// console.log(addDecl(2, 3));
+// // console.log(addExpr(2, 3));
+// // console.log(addArrow(2, 3));
+// function addDecl(a, b) {
+//   return a + b;
+// }
+
+// const addExpr = function (a, b) {
+//   return a + b;
+// };
+
+// var addArrow = (a, b) => a + b;
+
+// console.log(this);
+
+// const calcAge = function (year) {
+//   console.log(2037 - year);
+//   console.log(this);
+// };
+
+// calcAge(2003);
+
+// const calcAgeArr = year => {
+//   console.log(2037 - year);
+//   console.log(this);
+// };
+
+// calcAgeArr(2003);
+
+// const vitalii = {
+//   year: 2003,
+//   calcAge: function () {
+//     console.log(this);
+//     console.log(2024 - this.year);
+//   },
+// };
+
+// vitalii.calcAge();
+
+// const anna = {
+//   year: 2004,
+// };
+
+// anna.calcAge = vitalii.calcAge;
+
+// anna.calcAge();
+
+// const f = vitalii.calcAge;
+// f();
+
+// const vitalii = {
+//   firstname: 'vialii',
+//   year: 2003,
+//   calcAge: function () {
+//     //console.log(this);
+//     console.log(2024 - this.year);
+
+// const self = this;
+// const isMilenial = function () {
+//   // console.log(this.year >= 1981 && this.year <= 1996);
+//   console.log(self.year >= 1981 && self.year <= 1996);
+// };
+
+//     const isMilenial = () => {
+//       console.log(this);
+//       console.log(this.year >= 1981 && this.year <= 1996);
+//     };
+//     isMilenial();
+//   },
+
+//   greet: () => console.log(`Hey ${this.firstname}`),
+// };
+
+// vitalii.greet();
+// vitalii.calcAge();
+
+// let age = 20;
+
+// let oldAge = age;
+
+// age = 21;
+
+// console.log(age);
+// console.log(oldAge);
+
+// const me = {
+//   name: 'vitalii',
+//   age: 21,
+// };
+
+// const friend = me;
+// friend.age = 27;
+
+// console.log(me);
+// console.log(friend);
