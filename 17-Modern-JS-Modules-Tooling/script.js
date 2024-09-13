d8d0386728920710a3cc4e50d5f9c918a105a25b
VitaliiDiakiv <vitalii.diakiv.it@gmail.com>
2024-09-13 22:03:01 +0300

    Section 17

--- 17-Modern-JS-Modules-Tooling/script.js
+++ 17-Modern-JS-Modules-Tooling/script.js
@@ -1,3 +1,27 @@
-import { shippingCost, cart } from './shoppingCart.js';
-console.log('import');
-console.log(cart);
+// import { shippingCost, cart } from './shoppingCart.js';
+// console.log('import');
+// console.log(cart);
+
+const ShoppingCard = (function () {
+  const cart = [];
+  const shippingCost = 10;
+  const totalPrice = 12;
+  const totalQuantity = 4;
+
+  const addToCart = function (product, quantity) {
+    console.log(`${quantity} ${product} added to cart`);
+  };
+
+  const orderStack = function (product, quantity) {
+    console.log(`${quantity} ${product} order`);
+  };
+
+  return {
+    addToCart,
+    cart,
+    totalPrice,
+    totalQuantity,
+  };
+})();
+
+ShoppingCard.addToCart('apple', 3);
